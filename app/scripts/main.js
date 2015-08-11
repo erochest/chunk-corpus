73dbae0d9c3abf46ca14290a7ac1afabc0d6c7fe
Eric Rochester <erochest@virginia.edu>
2015-08-11 15:57:35 -0400

    Stub out the reactive pipeline.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,14 @@
-/* global jQuery
+/* global
+ * jQuery
+ * Bacon
+ * zip
  */
 
 
 (function($) {
   'use strict';
+
+  zip.workerScriptsPath = "/scripts/";
 
   var logAll = function(xs) {
     for (var i=0, x; x=xs[i]; i++) {
@@ -12,7 +17,9 @@
   };
 
   var outputFileName = function(file, tokenStart) {
-    file.name;
+  };
+
+  var updateOutputFileName = function(fileChunk) {
   };
 
   var tokenize = function(dataStr) {
@@ -21,45 +28,106 @@
   var chunk = function(xs, size, step) {
   };
 
-  var tokenizeFile = function(f, size, step) {
-    if (f.type == 'text/plain') {
-      var reader = new FileReader();
+  var readTextFile = function(f) {
+    var reader = new FileReader();
+    onLoad = Bacon.fromEvent(reader, 'load');
+    reader.readAsText(f, function(evt) {
+      return {
+        name: f.name,
+        contents: evt.target.result
+      };
+    });
+    return onLoad;
+  };
 
-      reader.onload = (function(file) {
-        return function(e) {
-          var tokens = tokenize(e.target.result);
-          var chunks = chunk(tokens, size, step);
-          for (var i=0, c; c=chunks[i]; i++) {
-            var output = outputFileName(file, c.start);
-            // TODO: write the output file
-          }
-        };
-      })(f);
+  var zipfile = function(z, f) {
+  };
 
-      reader.readAsText(f);
-    }
+  var finis = function(z) {
+  };
+
+  var forward = function(dataUrl) {
+  };
+
+  var over = function(property, f) {
+    return function(x) {
+      x[property] = f(x[property]);
+      return x;
+    };
+  };
+
+  var spread = function(property) {
+    return function(x) {
+      var xs    = x[property].slice();
+          names = Object.getOwnPropertyNames(x);
+
+      for (var i=0; i<xs.length; i++) {
+        var y = {};
+
+        for (var j=0, n; n=names[j]; j++) {
+          y[n] = x[n];
+        }
+
+        y[property] = xs[i];
+        xs[i] = y;
+      }
+
+      return Bacon.fromArray(xs);
+    };
   };
 
   $(function() {
-    $('.button-collapse').sideNav();
 
     if (window.File && window.FileReader && window.FileList && window.Blob) {
-      $('#input_files')
-        .on('change', function(evt) {
-          logAll(evt.target.files);
-        });
+      var fileInput  = $('#input_files'),
+          dropTarget = $('#drop_target'),
+          size       = parseInt($('#chunk_size').val()),
+          step       = parseInt($('#chunk_step').val()),
+          change     = null,
+          drop       = null,
+          inputFiles = null,
+          dropFiles  = null,
+          files      = null,
+          zipper     = null;
 
-      $('#drop_target')
-        .on('dragover', function(evt) {
+      change = fileInput.asEventStream('change');
+      inputFiles = change.flatMap(function(evt) {
+        return Bacon.fromArray(evt.target.files);
+      });
+
+      dropTarget.on('dragover', function(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        evt.originalEvent.dataTransfer.dropEffect = 'copy';
+      });
+      drop = dropTarget
+        .asEventStream('drop')
+        .doAction(function(evt) {
           evt.stopPropagation();
           evt.preventDefault();
-          evt.originalEvent.dataTransfer.dropEffect = 'copy';
-        })
-        .on('drop', function(evt) {
-          evt.stopPropagation();
-          evt.preventDefault();
-          logAll(evt.originalEvent.dataTransfer.files);
         });
+      dropFiles = drop.flatMap(function(evt) {
+        return Bacon.fromArray(evt.originalEvent.dataTransfer.files);
+      });
+
+      zipper = Bacon.mergeAll(change, drop).map(function(_) {
+        return new zip.BlobWriter();
+      }).toProperty();
+
+      files = Bacon.mergeAll(inputFiles, dropFiles)
+        .filter(function(f) { return f.type == 'text/plain'; })
+        .flatMap(readTextFile)
+        .map(over('contents', tokenize))
+        .map(over('contents', function(tokens) {
+          return chunk(tokens, size, step);
+        }))
+        .flatMap(spread('contents'))
+        .map(updateOutputFileName);
+
+      Bacon
+        .combineWith(zipfile, zipper, files)
+        .last(finis)
+        .doAction(forward);
 
     } else {
       $('#error_modal').openModal();
