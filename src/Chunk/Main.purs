e9f9efca0e2efeffd8dc5b6c32acf1fb2f8c2d1d
Eric Rochester <erochest@virginia.edu>
2015-08-27 14:06:28 -0400

    Working out types without enough information.

--- src/Chunk/Main.purs
+++ src/Chunk/Main.purs
@@ -2,8 +2,35 @@
 module Chunk.Main where
 
 
+import Control.Monad.Eff
 import Control.Monad.Eff.Console
+import Control.Bind
+import DOM.Node.NonDocumentTypeChildNode
+import DOM.HTML
+import DOM.HTML.Document
+import DOM.HTML.Window
+import DOM.Node.Document
+import DOM.Node.Types
+import DOM.HTML.Types
+import Prelude
+import Data.Nullable
+import Data.Maybe
 
 
-main = log "Chunkin' corpora left AND right! Yay me!"
+main :: forall eff. Eff eff Unit
+main = do
+  doc <-  return
+      <<< map htmlElementToElement
+      <<< toMaybe
+      =<< body
+      <<< document
+      =<< window
+  case doc of
+    Just doc' -> do
+      fileInput <- getElementById "input_files" doc'
+      log "#input_files"
+    Nothing -> log "No document"
 
+  fileInput <- getElementById "input_files" doc
+
+  log "Howdy!"
